126e6aa6e69f75d27412736f402e484145736d83
carlosandre-dev <andre.tjlba@gmail.com>
2023-05-29 18:54:44 -0300

    Method subtraction implementation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -15,6 +15,23 @@ static void Sum()
   float sum = valueOne + valueTwo;
 
   Console.WriteLine($"O resultado da soma de {valueOne} + {valueTwo} é: {sum}");
+  Console.ReadKey();
 }
 
+static void Subtraction()
+{
+  Console.Clear();
+  Console.WriteLine("Informe o primeiro valor:");
+  float valueOne = float.Parse(Console.ReadLine());
+
+  Console.WriteLine("Informe o segundo valor:");
+  float valueTwo = float.Parse(Console.ReadLine());
+
+  Console.WriteLine();
+
+  float subtraction = valueOne - valueTwo;
+
+  Console.WriteLine($"O resultado da subtração de {valueOne} - {valueTwo} é: {subtraction}");
+  Console.ReadKey();
+}
 
