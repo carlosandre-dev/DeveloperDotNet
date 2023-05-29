1ca9b4cd612842685662adb2731e10888fd396f7
carlosandre-dev <andre.tjlba@gmail.com>
2023-05-28 22:19:39 -0300

    Starting project Calculator - Part 1 Method Sum

---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+Console.WriteLine("Informe o primeiro valor:");
+float valueOne = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o segundo valor:");
+float valueTwo = float.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+float sum = valueOne + valueTwo;
+
+Console.WriteLine($"O resultado da soma de {valueOne} + {valueTwo} é: {sum}");
