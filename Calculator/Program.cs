1080ccc0d141fd0aca48307ca6b9176c014e4316
carlosandre-dev <andre.tjlba@gmail.com>
2023-05-29 17:30:00 -0300

    Update Method Sum

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,14 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Clear();
+Sum();
 
-Console.WriteLine("Informe o primeiro valor:");
-float valueOne = float.Parse(Console.ReadLine());
+static void Sum()
+{
+  Console.Clear();
+  Console.WriteLine("Informe o primeiro valor:");
+  float valueOne = float.Parse(Console.ReadLine());
 
-Console.WriteLine("Informe o segundo valor:");
-float valueTwo = float.Parse(Console.ReadLine());
+  Console.WriteLine("Informe o segundo valor:");
+  float valueTwo = float.Parse(Console.ReadLine());
 
-Console.WriteLine();
+  Console.WriteLine();
 
-float sum = valueOne + valueTwo;
+  float sum = valueOne + valueTwo;
 
-Console.WriteLine($"O resultado da soma de {valueOne} + {valueTwo} é: {sum}");
+  Console.WriteLine($"O resultado da soma de {valueOne} + {valueTwo} é: {sum}");
+}
+
+
