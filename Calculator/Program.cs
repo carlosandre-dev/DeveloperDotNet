db8c355ea482507fb3bc244ba128d7a16a48b72f
carlosandre-dev <andre.tjlba@gmail.com>
2023-05-29 18:56:52 -0300

    Method multiplication implementation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -35,3 +35,19 @@ static void Subtraction()
   Console.ReadKey();
 }
 
+static void Multiplication()
+{
+  Console.Clear();
+  Console.WriteLine("Informe o primeiro valor:");
+  float valueOne = float.Parse(Console.ReadLine());
+
+  Console.WriteLine("Informe o segundo valor:");
+  float valueTwo = float.Parse(Console.ReadLine());
+
+  Console.WriteLine();
+
+  float multiplication = valueOne * valueTwo;
+
+  Console.WriteLine($"O resultado da multiplicação de {valueOne} x {valueTwo} é: {multiplication}");
+  Console.ReadKey();
+}
